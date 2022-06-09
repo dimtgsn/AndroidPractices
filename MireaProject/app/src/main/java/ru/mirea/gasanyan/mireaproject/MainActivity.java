8e9a1148ab3f43479408be593f45e375c3f487eb
dmitry1721 <gasanyandmitry@yandex.ru>
2022-06-09 18:48:31 +0300

    Add MireaProject

--- MireaProject/app/src/main/java/ru/mirea/gasanyan/mireaproject/MainActivity.java
+++ MireaProject/app/src/main/java/ru/mirea/gasanyan/mireaproject/MainActivity.java
@@ -1,14 +1,90 @@
 package ru.mirea.gasanyan.mireaproject;
 
+import android.os.Bundle;
+import android.view.View;
+import android.view.Menu;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Button;
+
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.navigation.NavigationView;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import ru.mirea.gasanyan.mireaproject.databinding.ActivityMainBinding;
+import ru.mirea.gasanyan.mireaproject.ui.calculate.CalculateFragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AppBarConfiguration mAppBarConfiguration;
+    private ActivityMainBinding binding;
+
+//    CalculateFragment calculate = new CalculateFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        setSupportActionBar(binding.appBarMain.toolbar);
+        binding.appBarMain.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = binding.drawerLayout;
+        NavigationView navigationView = binding.navView;
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        mAppBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.nav_calculate)
+                .setOpenableLayout(drawer)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(navigationView, navController);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+
+//    protected void onSaveInstanceState(Bundle outState) {
+//        outState.putString("OPERATION", lastOperation);
+//        if(operand!=null)
+//            outState.putDouble("OPERAND", operand);
+//        super.onSaveInstanceState(outState);
+//    }
+//    // получение ранее сохраненного состояния
+//    @Override
+//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+//        super.onRestoreInstanceState(savedInstanceState);
+//        lastOperation = savedInstanceState.getString("OPERATION");
+//        operand= savedInstanceState.getDouble("OPERAND");
+//        resultField.setText(operand.toString());
+//        operationField.setText(lastOperation);
+//    }
+
+
 }
